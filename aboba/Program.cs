9c5e3f4ef54715b09e495c5150e5750d8e6a90e5
Dear Ilya <Dear Ilya@DESKTOP-EGLS4E4>
2024-04-12 15:52:52 +0500

    Добавьте файлы проекта.

---
+++ aboba/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace aboba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Process myProcess = new Process();
+
+            try
+            {
+                myProcess.StartInfo.UseShellExecute = false;
+                
+                myProcess.StartInfo.FileName = args[0];
+
+                myProcess.StartInfo.RedirectStandardInput = true;
+                myProcess.StartInfo.RedirectStandardOutput = true;
+
+
+                myProcess.Start();
+                Console.WriteLine("Дочерний процесс запущен. Ждём его окончания...");
+                // Ждём окончания дочернего процесса.
+                Console.Write("Введите что пинговать: ");
+                string ping = Console.ReadLine();
+                Console.Write("Введите количество пакетов: ");
+                string pac = Console.ReadLine();
+                myProcess.StandardInput.WriteLine("ping -n " + pac + " " + ping);
+                myProcess.StandardInput.WriteLine("exit");
+                StreamReader sr = myProcess.StandardOutput;
+                string line = sr.ReadToEnd();
+                line = line.Replace("Пакетов", "`");
+                line = line.Replace("Приблизительное", "`");
+                string[] str = line.Split('`');
+                Console.WriteLine("Пакетов" + str[1]);
+
+
+                
+                myProcess.WaitForExit();
+                Console.WriteLine("Дочерний процесс закончен.");
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
